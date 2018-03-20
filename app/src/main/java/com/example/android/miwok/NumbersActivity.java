4fa99e99bea5a6d5befb44ed4310354485b0edb7
Christian Montecillo <afchristian@gmail.com>
2018-03-19 22:36:06 -0500

    NumbersActivity after Lesson 2.8

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
@@ -11,22 +13,46 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        String [] wordsArray = new String[10];
+        // this was implemented before Lesson 2.7
+//        String [] wordsArray = new String[10];
+//
+//        wordsArray[0] = "one";
+//        wordsArray[1] = "two";
+//        wordsArray[2] = "three";
+//        wordsArray[3] = "four";
+//        wordsArray[4] = "five";
+//        wordsArray[5] = "six";
+//        wordsArray[6] = "seven";
+//        wordsArray[7] = "eight";
+//        wordsArray[8] = "nine";
+//        wordsArray[9] = "ten";
+//
+//        for (int i =0; i < 10; i++) {
+//            Log.v("Logging", "wordsArray[" + i + "]: " + wordsArray[i]);
+//        }
 
-        wordsArray[0] = "one";
-        wordsArray[1] = "two";
-        wordsArray[2] = "three";
-        wordsArray[3] = "four";
-        wordsArray[4] = "five";
-        wordsArray[5] = "six";
-        wordsArray[6] = "seven";
-        wordsArray[7] = "eight";
-        wordsArray[8] = "nine";
-        wordsArray[9] = "ten";
+        // this was implemented at Lesson 2.8
+        // change above wordsArray to an ArrayList
+
+        // create new ArrayList
+        ArrayList<String> wordsArrayList = new ArrayList<String>();
+
+        // add "one" through "ten" into ArraayList using .add
+        wordsArrayList.add("one");
+        wordsArrayList.add("two");
+        wordsArrayList.add("three");
+        wordsArrayList.add("four");
+        wordsArrayList.add("five");
+        wordsArrayList.add("six");
+        wordsArrayList.add("seven");
+        wordsArrayList.add("eight");
+        wordsArrayList.add("nine");
+        wordsArrayList.add("ten");
 
         for (int i =0; i < 10; i++) {
-            Log.v("Logging", "wordsArray[" + i + "]: " + wordsArray[i]);
+            Log.v("Logging", "wordsArrayList(" + i + "): " + wordsArrayList.get(i));
         }
+
     }
 
 }
